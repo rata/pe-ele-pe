aaea83f27283f03aaf7221d7e6d02516391eb738
kivielizalde <kivielizalde@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-05 21:24:48 +0000

    Implementar foldExp de lenguaje Lo unico que habia que hacer del .hs

--- trunk/tp1funcional/Lenguaje.hs
+++ trunk/tp1funcional/Lenguaje.hs
@@ -30,6 +30,11 @@ data FuncDef = FuncDef [VarId] Exp
 
 -- *** Ejercicio 2 ***
 
--- foldExp :: ???
-
+foldExp :: (Value -> b) -> (VarId -> b ) -> (Op -> b -> b -> b) -> ( b -> b -> b -> b) -> (VarId -> b -> b -> b) -> (FuncId  -> [b] -> b) -> Exp -> b
+foldExp fC _ _ _ _ _       (Const v)         = fC v
+foldExp _  fV _ _ _ _      (Var v)           = fV v
+foldExp fC fV fB fI fL fCa (BinOp o e1 e2)   = fB o (foldExp fC fV fB fI fL fCa e1) (foldExp fC fV fB fI fL fCa e2)
+foldExp fC fV fB fI fL fCa (IfZero e1 e2 e3) = fI (foldExp fC fV fB fI fL fCa e1) (foldExp fC fV fB fI fL fCa e2) (foldExp fC fV fB fI fL fCa e3)
+foldExp fC fV fB fI fL fCa (Let v e1 e2)     = fL v (foldExp fC fV fB fI fL fCa e1) (foldExp fC fV fB fI fL fCa e2)
+foldExp fC fV fB fI fL fCa (Call f e1)       = fCa f (map (foldExp fC fV fB fI fL fCa) e1)
 
