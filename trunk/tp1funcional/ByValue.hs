3c8c834f8b72237a84699c3bf4e252298c1ba85a
rodrigocc <rodrigocc@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-06 02:18:26 +0000

    Algo que no sirve y matias quiere commitear

--- trunk/tp1funcional/ByValue.hs
+++ trunk/tp1funcional/ByValue.hs
@@ -1,5 +1,6 @@
 --module ByValue(eval) where
 
+import Maybe
 import Lenguaje
 import Dict
 
@@ -15,31 +16,42 @@ type Environment = Dict VarId Value
 --eval :: ProgramDef -> Exp -> Value
 
 eval' :: ProgramDef -> Environment -> Exp -> Value
---eval' p en (Let v e1 e2) = foldExp id (getVar (extendDict en v e1)) binOp ifZ fLet call e
-eval' p en e = foldExp id (getVar en) binOp ifZ fLet call e
+eval' p en e = foldExp id (getVar en) binOp ifZ (fLet p en)  (call p en) e
 
 
 
-getVar :: Enviroment -> VarId -> Value
-getVar e v = maybe 0 id (lookupDict e v)
 -- Asigna cero a las variables que no estan definidas
+getVar :: Environment -> VarId -> Value
+getVar e v | a /= Nothing = fromJust a
+		where a = (lookupDict e v)
 
 binOp :: Op -> Value -> Value -> Value
-binOp op a b = binOp' op seq(b a) b
+binOp op a b = binOp' op (seq b a) b
 binOp' :: Op -> (Value -> Value -> Value)
-binOp' op | Add = (+)
- 	  | Mul = (*)
- 	  | Sub = (-)
+binOp' Add = (+)
+binOp' Mul = (*)
+binOp' Sub = (-)
 
-ifZ :: Value -> Value -> Value
-ifZ v1 v2 v3 = ifZ' seq ( v3 (seq v2 v1)) v2 v3
+ifZ :: Value -> Value -> Value -> Value
+ifZ v1 v2 v3 = ifZ' (seq v3 (seq v2 v1)) v2 v3
 
-ifZ' :: Value -> Value -> Value
+ifZ' :: Value -> Value -> Value -> Value
 ifZ' 0 v2 _  = v2
 ifZ' _ _ v3 = v3 
 
---fLet :: VarId -> Value -> (Environment -> Environment)
---fLet var v1 = (\e -> extendDict e var v1)
+fLet :: ProgramDef -> Environment -> VarId -> Value -> Exp -> Value
+fLet p en var v1 e1 = eval' p (extendDict en var v1) e1
 
-call
+call :: ProgramDef -> Environment -> FuncId -> [Value] ->Value
+call p en f vs e = eval' p nen (getExp (fromJust (lookupDict f p)))
+	where nen = foldr (\ t d -> extendDict d (fst t) (snd t)) en (toTupla (getParms (fromJust (lookupDict f p))) vs) 
+--	      defFunc = fromJust (lookupDict f p)
+
+getExp (FuncDef _ exp) = exp
+getParms (FuncDef ls _ ) = ls
+toTupla [] [] = []
+toTupla (x:xs) (y:ys) = (x,y):(toTupla xs ys)
+
 colgate = colgate
+
+test1 e colgate = e
