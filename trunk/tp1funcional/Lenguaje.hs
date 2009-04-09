5e75f30f5853734b7eddc971802a89625dd1d5b7
elmaildematiaz <elmaildematiaz@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-09 17:10:25 +0000

    Terminar By Value arreglando el foldExp Ademas hace DosToUnix de todos los arch

--- trunk/tp1funcional/Lenguaje.hs
+++ trunk/tp1funcional/Lenguaje.hs
@@ -30,11 +30,16 @@ data FuncDef = FuncDef [VarId] Exp
 
 -- *** Ejercicio 2 ***
 
-foldExp :: (Value -> b) -> (VarId -> b ) -> (Op -> b -> b -> b) -> ( b -> b -> b -> b) -> (VarId -> b -> Exp -> b) -> (FuncId  -> [b] -> b) -> Exp -> b
+
+foldExp :: (Value -> b) -> (VarId -> b ) -> (Op -> b -> b -> b) -> ( b -> b -> b -> b) ->  (VarId -> b -> b -> b) -> (FuncId  -> [b] -> b) -> Exp -> b
 foldExp fC _ _ _ _ _       (Const v)         = fC v
 foldExp _  fV _ _ _ _      (Var v)           = fV v
-foldExp fC fV fB fI fL fCa (BinOp o e1 e2)   = fB o (foldExp fC fV fB fI fL fCa e1) (foldExp fC fV fB fI fL fCa e2)
-foldExp fC fV fB fI fL fCa (IfZero e1 e2 e3) = fI (foldExp fC fV fB fI fL fCa e1) (foldExp fC fV fB fI fL fCa e2) (foldExp fC fV fB fI fL fCa e3)
-foldExp fC fV fB fI fL fCa (Let v e1 e2)     = fL v (foldExp fC fV fB fI fL fCa e1) e2
-foldExp fC fV fB fI fL fCa (Call f e1s)      = fCa f (map (foldExp fC fV fB fI fL fCa) e1s)
+foldExp fC fV fB fI fL fCa (BinOp o e1 e2)   = fB o (rec e1) (rec e2)
+	where rec = foldExp fC fV fB fI fL fCa
+foldExp fC fV fB fI fL fCa (IfZero e1 e2 e3) = fI (rec e1) (rec e2) (rec e3)
+	where rec = foldExp fC fV fB fI fL fCa
+foldExp fC fV fB fI fL fCa (Let v e1 e2)     = fL v (rec e1) (rec e2)
+	where rec = foldExp fC fV fB fI fL fCa
+foldExp fC fV fB fI fL fCa (Call f e1s)      = fCa f (map rec e1s)
+	where rec = foldExp fC fV fB fI fL fCa
 
