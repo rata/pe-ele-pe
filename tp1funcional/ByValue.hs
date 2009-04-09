dea3f5a8d4fcdc8d2c0847ab8ac4599c1f0ea31f
elmaildematiaz <elmaildematiaz@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-09 17:10:25 +0000

    Terminar By Value arreglando el foldExp Ademas hace DosToUnix de todos los arch

--- tp1funcional/ByValue.hs
+++ tp1funcional/ByValue.hs
@@ -1,4 +1,4 @@
---module ByValue(eval) where
+module ByValue(eval) where
 
 import Maybe
 import Lenguaje
@@ -9,49 +9,57 @@ type Environment = Dict VarId Value
 
 -- *** Ejercicios 3 y 4 ***
 
+-- Evalúa una expresión y devuelve el valor.
+eval :: ProgramDef -> Exp -> Value
+eval p e = eval' p e emptyDict
+
+eval' :: ProgramDef -> Exp -> Environment -> Value
+eval' p e en = (foldExp const getVar binOp ifZ fLet  (call p) e ) en
+
+
+getVar :: VarId -> Environment -> Value
+getVar v = \en -> fromJust (lookupDict en v)
+-- fromJust.(flip lookupDict) v
+
+--binOp :: Op -> (Environment -> Value) -> (Environment -> Value) -> (Environment -> Value)
+--binOp op a b = binOp' op (seq b a) b
+binOp :: Op -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
+--binOp Add f g en = (+) (f en) (g en)
+--binOp Mul f g en = (*) (f en) (g en)
+--binOp Sub f g en = (-) (f en) (g en)
+binOp Add f g = \en -> forceBV (+) (f en) (g en)
+binOp Mul f g = \en -> forceBV (*) (f en) (g en)
+binOp Sub f g = \en -> forceBV (-) (f en) (g en)
+--Hay que forzarlo a calcular los dos sumandos y luego sumar
+
+
+forceBV :: (a -> b -> c) -> a -> b -> c
+forceBV f a b = f (seq b a) b
+
+ifZ :: (Environment -> Value) -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
+ifZ v1 v2 v3 en = ifZ' (v1 en) (v2 en) (v3 en)
+
+ifZ' :: Value -> a -> a -> a
+ifZ' 0 v2 _  = v2
+ifZ' _ _  v3 = v3 
+
+fLet :: VarId -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
+fLet var f1 f2 en = f2 (extendDictByV en var (f1 en))
+
+call :: ProgramDef -> FuncId -> [(Environment -> Value)] -> Environment -> Value
+call p f fs en = eval' p (getExp defFunc) nen
+	where 	nen = foldr (\t d -> extendDictByV d (fst t) (snd t)) en (zip (getParms defFunc) vs)
+		vs = map (\a -> a en ) fs
+		defFunc = fromJust (lookupDict p f)
+
+extendDictByV :: Eq a => (Dict a b) -> a -> b -> (Dict a b)
+extendDictByV d k v = seq v (extendDict d k v)
+
 -- type ProgramDef = Dict FuncId FuncDef
 -- data FuncDef = FuncDef [VarId] Exp
 
--- Evalúa una expresión y devuelve el valor.
---eval :: ProgramDef -> Exp -> Value
-
-eval' :: ProgramDef -> Environment -> Exp -> Value
-eval' p en e = foldExp id (getVar en) binOp ifZ (fLet p en)  (call p en) e
-
-
-
--- Asigna cero a las variables que no estan definidas
-getVar :: Environment -> VarId -> Value
-getVar e v | a /= Nothing = fromJust a
-		where a = (lookupDict e v)
-
-binOp :: Op -> Value -> Value -> Value
-binOp op a b = binOp' op (seq b a) b
-binOp' :: Op -> (Value -> Value -> Value)
-binOp' Add = (+)
-binOp' Mul = (*)
-binOp' Sub = (-)
-
-ifZ :: Value -> Value -> Value -> Value
-ifZ v1 v2 v3 = ifZ' (seq v3 (seq v2 v1)) v2 v3
-
-ifZ' :: Value -> Value -> Value -> Value
-ifZ' 0 v2 _  = v2
-ifZ' _ _ v3 = v3 
-
-fLet :: ProgramDef -> Environment -> VarId -> Value -> Exp -> Value
-fLet p en var v1 e1 = eval' p (extendDict en var v1) e1
-
-call :: ProgramDef -> Environment -> FuncId -> [Value] ->Value
-call p en f vs = eval' p nen (getExp defFunc)
-	where nen = foldr (\t d -> extendDict d (fst t) (snd t)) en (toTupla (getParms defFunc) vs) 
-	      defFunc = fromJust (lookupDict p f)
-
+getExp :: FuncDef -> Exp
 getExp (FuncDef _ exp) = exp
+getParms :: FuncDef -> [VarId]
 getParms (FuncDef ls _ ) = ls
-toTupla [] [] = []
-toTupla (x:xs) (y:ys) = (x,y):(toTupla xs ys)
 
-colgate = colgate
-
-test1 e colgate = e
--- tp1funcional/Tests.hs
+++ tp1funcional/Tests.hs
@@ -2,6 +2,8 @@
 import Dict
 import Lenguaje
 import Parser
+import ByValue
+import Test.HUnit
 
 def :: String -> String -> (FuncId, FuncDef)
 def proto body = d (parser proto) (parser body)
@@ -23,10 +25,32 @@ definiciones = makeDict [
         " ifzero x then     \
         \   1               \
         \ else              \
-        \   x * fact(x - 1) "
+        \   x * fact(x - 1) ",
+	def "colgate(x)" 
+	"4 + colgate(x)",
+	def "primero(x,y)"
+	"x",
+	def "suma(x,y)"
+	"x + y"
   ]
 
 main1 = parser "fact(5)"
+main2 = parser "fib(4)"
+main3 = parser "primero(4,colgate(5))"
+main4 = parser "colgate(5)"
+main5 = parser "suma(7,primero(4,colgate(5)))"
+main6 = parser "ifzero 0 then \
+		\ 4 else colgate(3)"
 
---test1 = evaluar definiciones main1
+
+testBV = runTestTT test1
+test1 = test [eval definiciones main1  ~=? 120
+		, eval definiciones main2  ~=? 5 
+--		, eval definiciones main3  ~=? 4 
+--		, eval definiciones main4  ~=? 4 
+--		, eval definiciones main5  ~=? 4
+		, eval definiciones main6  ~=? 4  
+		 ]
+
+--test = eval' prog exp env
 
