a6db82b20d817cfef0ae9235f2e16d7dcef843cb
elmaildematiaz <elmaildematiaz@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-10 00:41:36 +0000

    primer intento de ByName

--- trunk/tp1funcional/ByName.hs
+++ trunk/tp1funcional/ByName.hs
@@ -11,8 +11,8 @@ type Substitution = Dict VarId Exp
 
 -- Extiende el dominio de la sustitución para que se
 -- pueda aplicar a cualquier expresión.
---substitute :: Exp -> Substitution -> Exp
---substitute e1 s = foldExp (\v -> Const v) gv gbinOp gifZ gLet gCall e1  s
+substitute :: Exp -> Substitution -> Exp
+substitute e1 s = foldExp (\v _ -> Const v) gv gbinOp gifZ gLet gCall e1 s
 
 gv :: VarId -> Substitution -> Exp
 gv a s	| isNothing v = Var a
@@ -41,12 +41,67 @@ gCall f es _ = Call f (map (\e -> (e emptyDict)) es )
 data Result exp = ReducesTo exp | Done
   deriving Show
 
---reduceOneStep :: ProgramDef -> Exp -> Result Exp
+reduceOneStep :: ProgramDef -> Exp -> Result Exp
+reduceOneStep p e = snd (reduceOneStep' p e)
 
+--rBinOp op (ReducesTo x) saraza = ReducesTo (BinOp op x saraza)
+--rBinOp op (Done x) saraza = BinOp op x saraza
+ 
+reduceOneStep' :: ProgramDef -> Exp -> (Exp, Result Exp)
+reduceOneStep' p e = foldExp (\v -> (Const v, Done)) (error "variable libre")  rBinOp rifZ rLet (rCall p) e
+
+rBinOp :: Op -> (Exp, Result Exp) -> (Exp, Result Exp) -> (Exp, Result Exp)
+rBinOp op (e1, ReducesTo x)  (e2, _) = ((BinOp op e1 e2), ReducesTo (BinOp op x e2))
+rBinOp op (e1, Done)  (e2, ReducesTo x) = ((BinOp op e1 e2), ReducesTo (BinOp op e1 x))
+rBinOp op (Const e1, Done)  (Const e2, Done) = ((BinOp op (Const e1) (Const e2)), ReducesTo (Const ((op2Func op) e1 e2)))
+
+-- Este podria estar en lenguaje y mejorar el binOp' de btvalue
+op2Func ::Num a => Op -> a -> a -> a
+op2Func Add = (+)
+op2Func Mul = (*)
+op2Func Sub = (-)
+
+rifZ :: (Exp, Result Exp) -> (Exp, Result Exp) -> (Exp, Result Exp) -> (Exp, Result Exp)
+rifZ (e1, ReducesTo x) (e2, _) (e3, _) = (IfZero e1 e2 e3, ReducesTo (IfZero x e2 e3))
+rifZ (Const 0, Done) (e2, _) (e3, _) = (IfZero (Const 0) e2 e3, ReducesTo e2)
+rifZ (e1, Done) (e2, _) (e3, _) = (IfZero e1 e2 e3, ReducesTo e2)
+
+
+rLet :: VarId -> (Exp, Result Exp) -> (Exp, Result Exp) -> (Exp, Result Exp)
+rLet v (e1, r1) (e2, r2) = (Let v e1 e2, ReducesTo (substitute e2 (makeDict [(v, e1)])))
+
+rCall :: ProgramDef -> FuncId -> [(Exp, Result Exp)] -> (Exp, Result Exp)
+rCall p f xs = (Call f params, g)
+	where   g = ReducesTo h
+		e = getExp defFunc
+		h = substitute e s
+		s = makeDict (zip (getParms defFunc) params)
+		defFunc = fromJust (lookupDict p f)
+		params = fst (unzip xs)
+
+
+getExp :: FuncDef -> Exp
+getExp (FuncDef _ exp) = exp
+getParms :: FuncDef -> [VarId]
+getParms (FuncDef ls _ ) = ls
 
 -- *** Ejercicio 7 ***
 
 -- Reduce la expresión a forma normal, aplicando
 -- reduceOneStep tantas veces como sea necesario.
---reduce :: ProgramDef -> Exp -> Value
+reduce :: ProgramDef -> Exp -> Value
+reduce p e = (getValue.res2Exp) (last (tk))
+	where   g (ReducesTo _) = True
+		g _             = False
+		getValue (Const x) = x
+		tk = takeWhile g (iterate ((reduceOneStep p).res2Exp) (ReducesTo e))
+				
+
+reduci :: ProgramDef -> Result Exp-> Result Exp
+reduci p (ReducesTo x) = reduceOneStep p x
+reduci p Done = Done
+
+
+res2Exp :: Result Exp -> Exp
+res2Exp (ReducesTo e) = e
 
