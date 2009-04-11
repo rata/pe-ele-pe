69110b8a6265d0a8fcbe5f97b3cfb2187000dbc6
elmaildematiaz <elmaildematiaz@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-11 17:29:15 +0000

    Test.hs Casos de test 'representativos'

--- trunk/tp1funcional/Tests.hs
+++ trunk/tp1funcional/Tests.hs
@@ -14,36 +14,49 @@ def proto body = d (parser proto) (parser body)
 definiciones :: ProgramDef
 definiciones = makeDict [
 
-    def "fib(x)"
-        " ifzero x then             \
-        \   1                       \
-        \ else ifzero x-1 then      \
-        \   1                       \
-        \ else                      \
-        \   fib(x - 1) + fib(x - 2) ",
+	def "fib(x)"
+        	" ifzero x then             \
+	        \   1                       \
+	        \ else ifzero x-1 then      \
+	        \   1                       \
+	        \ else                      \
+	        \   fib(x - 1) + fib(x - 2) ",
 
-    def "fact(x)"
-        " ifzero x then     \
-        \   1               \
-        \ else              \
-        \   x * fact(x - 1) ",
+	def "fact(x)"
+	        " ifzero x then     \
+	        \   1               \
+	        \ else              \
+        	\   x * fact(x - 1) ",
+
 	def "colgate(x)" 
-	"4 + colgate(x)",
+		"4 + colgate(x)",
+
 	def "primero(x,y)"
-	"x",
+		"x",
+
 	def "suma(x,y)"
-	"x + y",
+		"x + y",
+
 	def "leticia(x,y,z)"
-	"let x=4 in x + suma(x,z) * y",
+		"let x=4 in x + suma(x,z) * y",
+
 	def "sellama(x)"
-	" ifzero x  	\
-	\ then 3 	\
-	\ else 		\
-	\ 2",
+		" ifzero x  	\
+		\ then 3 	\
+		\ else 		\
+		\ 2",
+	
 	def "varLibre(y)"
-	"y + y"
+		"y + y" ,
+	def "llamaA(x)"
+		"ifzero x	\
+		\ then 0	\
+		\ else 1 + llamaB(x-1) ",
+	def "llamaB(x)"
+		"1 * llamaA(x)"
   ]
 
+main0 = parser "7"
 main1 = parser "fact(5)"
 main2 = parser "fib(4)"
 main3 = parser "primero(4,colgate(5))"
@@ -58,29 +71,16 @@ main10 = parser "let x=5 in leticia(1,x,3)"
 main11 = parser "sellama(2)"
 main12 = parser "suma(2,3)"
 main13 = parser "let y=3 in varLibre(2)"
-
+main14 = parser "( fact(4) * 5 ) + 30"
+main15 = parser "llamaA(8)"
 
 testBV = (runTestTT.test) testCBV
 testBN = (runTestTT.test) testCBN
 
-test1 = test [ eval definiciones main1 ~=?  120
-		, eval definiciones main2  ~=? 5 
---		, eval definiciones main3  ~=? 4 
---		, eval definiciones main4  ~=? 4 
---		, eval definiciones main5  ~=? 4
-		, eval definiciones main6  ~=? 4  
-		, eval definiciones main7  ~=? 4
-		, eval definiciones main8  ~=? 6
-		, eval definiciones main9  ~=? 18
-		, eval definiciones main10  ~=? 39
-		 ]
-
 testcases :: [(ProgramDef, Exp, Int)]
-testcases =  [ ( definiciones, main1,  120)
+testcases =  [	  ( definiciones, main0,  7)
+		, ( definiciones, main1,  120)
 		, ( definiciones, main2  , 5)
---		, ( definiciones, main3, 4) 
-		, ( definiciones, main4, 4) 
-		, ( definiciones, main5, 11)
 		, ( definiciones, main6 , 3) 
 		, ( definiciones, main7 , 4)
 		, ( definiciones, main8 , 6)
@@ -89,7 +89,13 @@ testcases =  [ ( definiciones, main1,  120)
 		, ( definiciones, main11, 2)
 		, ( definiciones, main12, 5)
 		, ( definiciones, main13, 4)
-		 ]
+		, ( definiciones, main14, 150)
+		, ( definiciones, main15, 8)
+--Casos que se cuelgan
+		, ( definiciones, main5, 11)
+		, ( definiciones, main3, 4) 
+		, ( definiciones, main4, 4)
+	 	 ]
 
 testCBV = map (\(a,b,c) -> (eval a b ~=? c) ) testcases
 --test = eval' prog exp env
