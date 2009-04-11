e1f648ddf65d6c5943a2cdaffec65e95a0b43197
elmaildematiaz <elmaildematiaz@441168e2-205a-11de-ac9f-1d9250355bca>
2009-04-11 19:08:37 +0000

    ByValue.hs Usando mapByV y makeDict :) (Y andando :P)

--- trunk/tp1funcional/ByValue.hs
+++ trunk/tp1funcional/ByValue.hs
@@ -16,16 +16,13 @@ eval p e = eval' p e emptyDict
 eval' :: ProgramDef -> Exp -> Environment -> Value
 eval' p e en = foldExp const getVar binOp ifZ fLet (call p) e en
 
-
 getVar :: VarId -> Environment -> Value
-getVar v = \en -> fromJust (lookupDict en v)
+getVar v en = fromJust (lookupDict en v)
 
-binOp :: Op -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
-binOp op f g = \en -> forceBV (op2Func op) (f en) (g en)
 --Hay que forzarlo a calcular los dos sumandos y luego sumar
-
-forceBV :: (a -> b -> c) -> a -> b -> c
-forceBV f a b = f (seq b a) b
+binOp :: Op -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
+binOp op f g en = forceBV (op2Func op) (f en) (g en)
+	where forceBV f a b = f (seq b a) b
 
 ifZ :: (Environment -> Value) -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
 ifZ v1 v2 v3 en = ifZ' (v1 en) (v2 en) (v3 en)
@@ -37,13 +34,18 @@ ifZ' _ _  v3 = v3
 
 fLet :: VarId -> (Environment -> Value) -> (Environment -> Value) -> Environment -> Value
 fLet var f1 f2 en = f2 (extendDictByV en var (f1 en))
+	where extendDictByV d k v = seq v (extendDict d k v)
 
 call :: ProgramDef -> FuncId -> [(Environment -> Value)] -> Environment -> Value
 call p f fs en = eval' p (getExp defFunc) nen
-	where 	nen = foldr (\t d -> extendDictByV d (fst t) (snd t)) emptyDict (zip (getParms defFunc) vs)
-		vs = map (\a -> a en ) fs
-		defFunc = fromJust (lookupDict p f)
+	where	defFunc = fromJust (lookupDict p f)
+		nen = makeDict (zip (getParms defFunc) vs)
+		vs = mapByV (\a -> a en) fs
 
-extendDictByV :: Eq a => (Dict a b) -> a -> b -> (Dict a b)
-extendDictByV d k v = seq v (extendDict d k v)
+mapByV :: (a -> b) -> [a] -> [b]
+mapByV f xs = foldr g [] xs
+	where g a b = seq (f a) ((f a):b)
 
+--mapByV :: (a -> b) -> [a] -> [b]
+--mapByV f xs = map g xs
+--	where g a = seq (f a) (f a)
