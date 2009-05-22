e2f30a049e2a2a98460f7e65098928517582fdfa
elmaildematiaz <elmaildematiaz@441168e2-205a-11de-ac9f-1d9250355bca>
2009-05-22 18:30:31 +0000

    Se agrega el tp 2 de programacion logica

---
+++ trunk/tp2logica/solucion.pl
@@ -0,0 +1,49 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+:- consult(['mostrar', 'tests']).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%% Ejercicios %%%%
+
+%% Ejercicio 1
+
+% dimension(+Tablero, -N, -M).
+% posicion(+Tablero, -I, -J).
+
+
+%% Ejercicio 2
+
+% subtablero(+Tablero, +I, +J, -Subtablero).
+
+
+%% Ejercicio 3
+
+% transponer(+Tablero, -Transpuesto).
+
+
+%% Ejercicio 4
+
+% asignarPeso(+Silueta, +Peso, -SiluetaConPeso).
+
+
+%% Ejercicio 5
+
+% ubicarSilueta(+Silueta, -I, -J, -Tablero).
+
+
+%% Ejercicio 6
+
+% ubicarPieza(+NombrePieza, +Peso, +DiccionarioPiezas, -I, -J, -Tablero).
+
+
+%% Ejercicio 7
+% solucionValida(-Juego).
+
+
+%% Ejercicio 8
+% resolver(+DiccionarioPiezas, +PiezasDisponibles, -Juego).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
