0b642ef0d3b388493d7db3d6477b504e4070ae1d
kivielizalde <kivielizalde@441168e2-205a-11de-ac9f-1d9250355bca>
2009-05-24 23:13:22 +0000

    Implementar Ej 2 y 3

--- tp2logica/solucion.pl
+++ tp2logica/solucion.pl
@@ -10,20 +10,51 @@
 %% Ejercicio 1
 
 %dimension(+Tablero, -N, -M).
-dimension(Tablero, N, M):- length(Tablero, N), nth0(0, Tablero, L), length(L, M).
+dimension(T, N, M):- length(T, N), nth0(0, T, L), length(L, M).
+%dimension(T, N, M):- length(T, N) , N1 is N-1, forall( between(0,N1,X), (nth0(X,T,L), length(L, M)) ).
+%dimension(T, N, M):- length(T, N) , forall( member(X,T), length(X,M) ).
+
 % posicion(+Tablero, -I, -J).
-posicion(Tablero, I, J):- dimension(Tablero, N, M), between(0, N, I), between(0, M, J).
+posicion(T, I, J):- dimension(T, N, M), between(0, N, I), between(0, M, J).
 
 
 %% Ejercicio 2
 
 % subtablero(+Tablero, +I, +J, -Subtablero).
+%subtablero(T, I, J, S):- dimension(T,N,M), NewN is N-I, NewM is M-J, N1 is N-1, M1 is M-1 ,dimension(S, NewN, NewM), 
+%		forall(between(I,N1,F),forall(between(J,M1,C),(
+%					FI is F-I,
+%					CJ is C-J,
+%					nth0(T,F,X1),
+%					nth0(X1,C,X2),
+%			       		nth0(S,FI,Y1),
+%					nth0(Y1,CJ,X2)))).
 
+
+% subtablero(+Tablero, +I, +J, -Subtablero).
+subtablero(T, I, J, L):- dimension(T, N, M), I >= N, L = [].
+subtablero(T, I, J, L):- dimension(T, N, M), I < N, I2 is I+1, subtablero(T, I2, J, L2), nth0(I, T, Fila),
+					sacarN(J, Fila, Col), L = [Col | L2].
+
+% sacarN(+N, +L, -L2)
+sacarN(0, L1, L1).
+sacarN(N, [L|L1], L2):- N1 is N-1, sacarN(N1, L1, L2).
 
 %% Ejercicio 3
 
 % transponer(+Tablero, -Transpuesto).
+transponer(T, Trans):- transponerAux(T, Trans, 0).
 
+
+transponerAux(T,Trans,I):- dimension(T, N, M), I>=M, Trans=[].
+transponerAux(T,Trans,I):- dimension(T, N, M), I< M, I1 is I+1, transponerAux(T, Rec, I1), getCol(T, I, Col),
+				Trans = [Col|Rec].
+
+%getCol(T,C,L):- dimension(T,N,M), C < M, getColAux(T,C,0,L).
+getCol(T,C,L):- getColAux(T,C,0,L).
+
+getColAux(T,C,I,L):- dimension(T,N,M), I>=N, L=[].
+getColAux(T,C,I,L):- dimension(T,N,M), I< N, I1 is I+1, getColAux(T, C, I1, L1), nth0(I, T, F), nth0(C, F, R), L = [R|L1].
 
 %% Ejercicio 4
 
