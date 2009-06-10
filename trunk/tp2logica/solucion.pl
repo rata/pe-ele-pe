371ece09610e4a46c275c8889853223729cacf2d
rodrigocc <rodrigocc@441168e2-205a-11de-ac9f-1d9250355bca>
2009-06-10 04:02:41 +0000

    Retoques de ultimo momento
    Lo hizo matias, no quiero robarme el credito, pero quiero que este commiteado :)
    
    Saca las "otra forma de implementarlo" que estaban comentadas y demas boludeces
    para que quede apto para entregarlo.

--- trunk/tp2logica/solucion.pl
+++ trunk/tp2logica/solucion.pl
@@ -1,22 +1,16 @@
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 :- consult(['mostrar', 'tests']).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%% Ejercicios %%%%
 
 %% Ejercicio 1
 
 %dimension(+Tablero, -N, -M).
 dimension(T,N,M):- nonvar(T), dimensionNonvar(T,N,M).
 dimension(T,N,M):- var(T), dimensionVar(T,N,M).
+
 dimensionNonvar(T, N, M):- length(T, N), nth0(0, T, L), length(L, M).
+
 dimensionVar(T, N, M):- length(T, N), repetir(N,M,X), maplist(length,T,X).
-%dimension(T, N, M):- length(T, N) , N1 is N-1, forall( between(0,N1,X), (nth0(X,T,L), length(L, M)) ).
-%dimension(T, N, M):- length(T, N), nth0(0,T,L), length(L,M) ,forall( member(X,T), length(X,M) ).
-%dimension(T, N, M):- length(T, N), nth0(0,T,L), length(L,M), checkCols(T,1,N,L).
+
 
 repetir(0,N,[]).
 repetir(I,N,[N|M]):- I > 0, I1 is I-1, repetir(I1,N,M).
@@ -26,17 +20,6 @@ posicion(T, I, J):- dimension(T, N, M), N1 is N-1, M1 is M-1, between(0, N1, I),
 
 
 %% Ejercicio 2
-
-% subtablero(+Tablero, +I, +J, -Subtablero).
-%subtablero(T, I, J, S):- dimension(T,N,M), NewN is N-I, NewM is M-J, N1 is N-1, M1 is M-1 ,dimension(S, NewN, NewM), 
-%		forall(between(I,N1,F),forall(between(J,M1,C),(
-%					FI is F-I,
-%					CJ is C-J,
-%					nth0(T,F,X1),
-%					nth0(X1,C,X2),
-%			       		nth0(S,FI,Y1),
-%					nth0(Y1,CJ,X2)))).
-
 
 % subtablero(+Tablero, +I, +J, -Subtablero).
 subtablero(T, I, J, L):- dimension(T, N, M), I >= N, L = [].
@@ -52,7 +35,6 @@ sacarN(N, [L|L1], L2):- N1 is N-1, sacarN(N1, L1, L2).
 % transponer(+Tablero, -Transpuesto).
 transponer(T, Trans):- transponerAux(T, Trans, 0).
 
-
 transponerAux(T,Trans,I):- dimension(T, N, M), I>=M, Trans=[].
 transponerAux(T,Trans,I):- dimension(T, N, M), I< M, I1 is I+1, transponerAux(T, Rec, I1), getCol(T, I, Col),
 				Trans = [Col|Rec].
@@ -66,13 +48,6 @@ getColAux(T,C,I,L):- dimension(T,N,M), I< N, I1 is I+1, getColAux(T, C, I1, L1),
 %% Ejercicio 4
 
 % asignarPeso(+Silueta, +Peso, -SiluetaConPeso).
-%asignarPeso(S, P, SP):- %dimension(S,F,C),F1 is F-1, C1 is C-1, 
-%			forall(
-%				(between(0,1,I), between(0,1,J), elemento(S,I,J,E)), 
-%						nonvar(E) -> elemento(SP,I,J,P) ; true 
-%				).
-
-% asignarPeso(+Silueta, +Peso, -SiluetaConPeso).
 asignarPeso(S, P, SP):- asignarAux(S,0,P,SP).
 
 asignarAux(S,I,P,SP):- dimension(S,N,M), I>=N, SP=[].
@@ -84,17 +59,14 @@ reformado([L|LS],P,[C|CS]):- reformado(LS,P,CS), ((nonvar(L), C = P )| var(L)).
 
 elemento(S,I,J,E):- nth0(I,S,R), nth0(J,R,E).
 
-
 %% Ejercicio 5
 
 %ubicarSilueta(+Silueta, -I, -J, -Tablero).
 ubicarSilueta(S, I, J, T):- posicion(T,I,J), subtablero(T,I,J,ST), entraSiluetaArriba(S,ST).
 
 entraSiluetaArriba(S,T):- length(S,LS), length(T,LT), LS =< LT, L is LT-LS, recortar(T,L,TR), maplist(entraFila,S,TR).
-%entraSiluetaArriba(S,T):- length(S,LS), length(T,LT), LS = LT, maplist(entraFila,S,T).
 
 entraFila(S,T):- length(S,LS), length(T,LT), LS =< LT, L is LT-LS, recortar(T,L,TR), maplist(entraCelda,S,TR).
-%entraFila(S,T):- length(S,LS), length(T,LT), LS = LT, maplist(entraCelda,S,T).
 
 %recortar(+List,+N,?List)
 recortar(L,N,R):- reverse(L,RL), sacarN(N,RL,RLR), reverse(RLR,R).
@@ -115,18 +87,15 @@ matchFila(S,T):- length(S,LS), length(T,LT), LS =< LT, L is LT-LS, recortar(T,L,
 
 igualCelda(X,Y):- nonvar(X) -> Y is X ; true.
 
-
 %% Ejercicio 7
+
 % solucionValida(-Juego).
-solucionValida(juego(T,RF,RC)):- dimension(T,N,M), length(RF,N), length(RC,M), maplist(sumlist,T,RF), transponer(T,TT), maplist(sumlist,TT,RC).
+solucionValida(juego(T,RF,RC)):- tableroLleno(T) ,dimension(T,N,M), length(RF,N), length(RC,M), maplist(sumlist,T,RF), transponer(T,TT), maplist(sumlist,TT,RC).
+
+tableroLleno([]).
+tableroLleno(T):- not(ubicarSilueta([[1]],I,J,T)) 
 
 %% Ejercicio 8
 % resolver(+DiccionarioPiezas, +PiezasDisponibles, -Juego).
-%resolver(DiccP, PDisp, juego(T,RF,RC)):- forall(member(pieza(Nom,Peso),PDisp) , ubicarPieza(Nom,Peso,DiccP,I,J,T) ), solucionValida(juego(T,RF,RC)) .
-
 resolver(DiccP, [pieza(Nomb,Peso)|PDisp], juego(T,RF,RC)):- ubicarPieza(Nom,Peso,DiccP,I,J,T), resolver(DiccP, PDisp, juego(T,RF,RC)) .
 resolver(DiccP, [], juego(T,RF,RC)):- solucionValida(juego(T,RF,RC)).
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
